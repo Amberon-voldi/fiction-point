a758047faac1a21a010c79c503ce10fb9099f76c
Amberon-voldi <ambujpandey742@gmail.com>
2024-07-24 00:21:55 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    
+    images: {
+        domains: ['cdn.pixabay.com', 'us-a.tapas.io'],
+    },
+
+};
 
 export default nextConfig;
